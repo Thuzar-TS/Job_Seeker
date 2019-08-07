3bb9762af0a6d8eee679be5213a43339867df0bf
mawutmoneoo <mawutmoneoo@gmail.com>
2019-08-07 14:00:19 +0630

    Layout changed

--- layout/nav.php
+++ layout/nav.php
@@ -7,9 +7,10 @@
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Logout</a></li>                
+            <ul class="nav navbar-top-links navbar-right">
+				<a href="#" class="logoutBtn"><i class="glyphicon glyphicon-log-out side-icon"></i> ログアウト</a>
             </ul>
+        
         </div>
     </div>
 </nav>
--- pages/A-1_Top.php
+++ pages/A-1_Top.php
@@ -2,6 +2,34 @@
 
         <div class="wrap">
           <div class="panel panel-info">
+          <div class="row" style="margin-left: 50px;margin-right: 50px;">
+            <div class="col-sm-12">
+              <div class="panel-heading ">
+              <h4 class="headingStyle">顧客番号、顧客名、求人番号 で求人を検索できます</h4>
+              
+              </div>            
+            </div>
+            <div class="col-sm-12">
+            <form action="/hms/accommodations" method="GET"> 
+            <div class="row">
+                <div class="col-xs-6 col-md-12">
+                  
+                  <div class="col-xs-6 col-md-10">
+                    <input type="text" class="form-control" placeholder="検索" id="txtSearch">
+                  </div>
+                  <div class="col-xs-6 col-md-2">
+                    
+                      <button class="btn btn-primary col-md-12 searchBtn" type="submit">
+                        <span class="glyphicon glyphicon-search side-icon"></span> 検索
+                      </button>
+                
+                  </div>
+                
+                </div>
+              </div>
+            </form>
+            </div>
+          </div>
             <h4><a href="">顧客情報管理</a></h4>
 
             <ul>
