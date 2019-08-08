bedb4fc6dc280da78432d652b89dc6144e810161
zinkomyint <zinkomyint.gamer@gmail.com>
2019-08-08 10:09:11 +0630

    customer_create_view

--- pages/Customer_view.php
+++ pages/Customer_view.php
@@ -2,17 +2,24 @@
 <link rel="stylesheet" type="text/css" href="../css/style.css">
 
         <div class="wrap">
-        <div class="container" style="margin-top:40px;">    
+        
+        <div class="container" style="margin-top:40px;"> 
+        
+        
                 <div class="jumbotron" style="margin-top:40px;background-color:white;">
                   <div class="row">
+                  <button class="btn btn-info pull-right">バック</button>
                       <div class="col-lg-4 text" style="margin-top:30px;">
+                      
                           <img src="../images/profile.png" alt="stack photo" class="img" width="250px" height="250px">
                       </div>
                       <div class="col-lg-8">
                           <div class="container" style="border-bottom:1px solid black;">
                             <h3>顧客情報</h3>
-                          </div>
-                            <hr>
+                           
+                          </div><br>
+                          
+                           
                           <ul class="container details" >
                             <li ><p style="font-size:19px;letter-spacing:2px;" class="text"><span class="glyphicon glyphicon-user" style="width:50px;"></span>顧客名</p></li>
                             <li ><p style="font-size:19px;letter-spacing:2px;" class="text"><span class="glyphicon glyphicon-briefcase" style="width:50px;"></span>顧客ID</p></li>
